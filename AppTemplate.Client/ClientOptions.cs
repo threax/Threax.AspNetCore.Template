9b1cac391166a82bd475a1d31a832dd1f54275d3
Andrew Piper <andrew@threax.com>
2020-02-11 18:42:53 -0500

    Updated to new halcyon client.

--- AppTemplate.Client/ClientOptions.cs
+++ AppTemplate.Client/ClientOptions.cs
@@ -9,10 +9,5 @@ namespace AppTemplateClient
         /// The url of the service.
         /// </summary>
         public string ServiceUrl { get; set; }
-
-        /// <summary>
-        /// The options when using ClientCredentials, otherwise ignored.
-        /// </summary>
-        public ClientCredentailsAccessTokenFactoryOptions ClientCredentials { get; set; } = new ClientCredentailsAccessTokenFactoryOptions();
     }
 }
--- AppTemplate.Client/DiExtensions.cs
+++ AppTemplate.Client/DiExtensions.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppTemplateClient;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using AppTemplateClient;
 using System;
-using Threax.AspNetCore.AuthCore;
 using Threax.AspNetCore.Halcyon.Client;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -15,46 +13,18 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="services">The service collection.</param>
         /// <param name="configure">The configure callback.</param>
         /// <returns></returns>
-        public static IServiceCollection AddAppTemplateWithClientCredentials(this IServiceCollection services, Action<AppTemplateOptions> configure)
+        public static IHalcyonClientSetup<EntryPointInjector> AddAppTemplateClient(this IServiceCollection services, Action<AppTemplateOptions> configure)
         {
             var options = new AppTemplateOptions();
             configure?.Invoke(options);
 
-            services.TryAddSingleton<IHttpClientFactory, DefaultHttpClientFactory>();
-            services.TryAddSingleton<IHttpClientFactory<EntryPointInjector>>(s =>
-            {
-                return new ClientCredentialsAccessTokenFactory<EntryPointInjector>(options.ClientCredentials, new BearerHttpClientFactory<EntryPointInjector>(s.GetRequiredService<IHttpClientFactory>()));
-            });
+            services.TryAddSingleton<IHttpClientFactory<EntryPointInjector>, DefaultHttpClientFactory<EntryPointInjector>>();
             services.TryAddScoped<EntryPointInjector>(s =>
             {
                 return new EntryPointInjector(options.ServiceUrl, s.GetRequiredService<IHttpClientFactory<EntryPointInjector>>());
             });
 
-            return services;
-        }
-
-        /// <summary>
-        /// Add the AppTemplate setup to forward the current user's access token from the to the service.
-        /// </summary>
-        /// <param name="services">The service collection.</param>
-        /// <param name="configure">The configure callback.</param>
-        /// <returns></returns>
-        public static IServiceCollection AddAppTemplateWithUserAccessToken(this IServiceCollection services, Action<AppTemplateOptions> configure)
-        {
-            var options = new AppTemplateOptions();
-            configure?.Invoke(options);
-
-            services.TryAddSingleton<IHttpClientFactory, DefaultHttpClientFactory>();
-            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.TryAddScoped<IHttpClientFactory<EntryPointInjector>>(s =>
-                new AddUserTokenHttpClientFactory<EntryPointInjector>(p => p.GetAccessToken(), s.GetRequiredService<IHttpContextAccessor>(), s.GetRequiredService<IHttpClientFactory>(), s.GetRequiredService<ILoggingInUserAccessor>()));
-            services.TryAddScoped<ILoggingInUserAccessor, LoggingInUserAccessor>();
-            services.TryAddScoped<EntryPointInjector>(s =>
-            {
-                return new EntryPointInjector(options.ServiceUrl, s.GetRequiredService<IHttpClientFactory<EntryPointInjector>>());
-            });
-
-            return services;
+            return new HalcyonClientSetup<EntryPointInjector>(services);
         }
     }
 }
