35fa7ea9a746415d296f98032eaedbf78a134d1a
Andrew Piper <andrew@threax.com>
2020-08-09 13:56:20 -0400

    Updated nuget packages.

--- AppTemplate/Startup.cs
+++ AppTemplate/Startup.cs
@@ -98,10 +98,7 @@ namespace AppTemplate
                 o.AppOptions = authConfig;
                 o.CookiePath = appConfig.PathBase;
                 o.AccessDeniedPath = "/Account/AccessDenied";
-                o.ConfigureIdServerMetadataJwtOptions = jwtOpt =>
-                {
-                    jwtOpt.Audience = "Threax.IdServer";
-                };
+                o.EnableIdServerMetadata = appConfig.EnableIdServerMetadata;
                 o.CustomizeCookies = cookOpt =>
                 {
                     cookOpt.BearerHttpOnly = false;
