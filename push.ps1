3e0e1ca8decafaeb42d61557b523091431bcdb30
Andrew Piper <andrew@threax.com>
2021-01-19 14:47:39 -0500

    Remove push.

--- push.ps1
+++
@@ -1,4 +0,0 @@
-git push
-git push https://github.com/threax/Threax.AspNetCore.Template.git
-git push https://gitlab.com/threax-software/Threax.AspNetCore.Template.git
-git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template
