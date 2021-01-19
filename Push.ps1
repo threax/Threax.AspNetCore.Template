6ea35a191bf46729e08bfa89ffc3a55483fb8bd6
Andrew Piper <andrew@threax.com>
2021-01-19 14:47:51 -0500

    Rename push.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/Threax.AspNetCore.Template.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.Template.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template
