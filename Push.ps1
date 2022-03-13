ffe2572cc2a8d8b17ba58db3528e8b047e99fe0a
Andrew Piper <andrew@threax.com>
2022-03-13 14:02:50 -0400

    Add tags to push script.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,8 @@
 git push
+git push --tags
 git push https://github.com/threax/Threax.AspNetCore.Template.git
+git push https://github.com/threax/Threax.AspNetCore.Template.git --tags
 git push https://gitlab.com/threax-software/Threax.AspNetCore.Template.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.Template.git --tags
 git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Template --tags
